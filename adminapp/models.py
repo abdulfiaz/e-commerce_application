a2f4f944fabde4cf14bc24b101463490c7e42ebf
muthukumaran1234 <smuthukuma498@gmail.com>
2024-10-03 12:35:57 +0530

    [ADD]customuser model in adminapp

---
+++ adminapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    id=models.BigIntegerField(primary_key=True)
+    first_name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(unique=True)  
+    password = models.CharField(max_length=255, null=True, blank=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
