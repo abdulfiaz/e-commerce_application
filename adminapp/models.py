4a104dca152988afbe21cfc80fcda28da1671e81
muthukumaran1234 <smuthukuma498@gmail.com>
2024-10-04 14:40:38 +0530

    [ADD]tables IUMaster,RoleMaster,UserRoleMapping

--- adminapp/models.py
+++ adminapp/models.py
@@ -1,14 +1,68 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+class IUMaster(models.Model):
+    name=models.CharField(max_length=100,blank=True,null=True)
+    description=models.CharField(max_length=100,blank=True,null=True)
+    host=ArrayField(models.CharField(max_length=200,blank=True,null=True))  
+    is_active=models.BooleanField(default=True)
+    created_by=models.IntegerField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    modified_by=models.IntegerField()
+    modified_at=models.DateTimeField(auto_now=True)
+
+    
+    class Meta:
+        db_table="IUMaster"
+
 class CustomUser(AbstractUser):
-    id=models.BigIntegerField(primary_key=True)
+    
     first_name = models.CharField(max_length=255, null=True, blank=True)
     last_name = models.CharField(max_length=255, null=True, blank=True)
+    username=models.CharField(max_length=100,null=True,blank=True)
     email = models.EmailField(unique=True)  
     password = models.CharField(max_length=255, null=True, blank=True)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    mobile_number = models.CharField(max_length=15, blank=True, null=True)
     date_of_birth = models.DateField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    created_by=models.IntegerField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    modified_by=models.IntegerField()
+    modified_at=models.DateTimeField(auto_now=True)
+    iu_id = models.ForeignKey(IUMaster,on_delete=models.CASCADE)
+    temp_otp=models.IntegerField(blank=True,null=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['mobile_number']
+
+    class Meta:
+        db_table="CustomUser"
+        ordering = ["created_at"]
+        unique_together=[('mobile_number','iu_id')]
+
+class RoleMaster(models.Model):
+    role_name=models.CharField(max_length=100,blank=True,null=True)
+    role_description=models.CharField(max_length=100,blank=True,null=True)
+    is_active=models.BooleanField(default=True)
+    created_by=models.IntegerField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    modified_by=models.IntegerField()
+    modified_at=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table="RoleMaster"
+        ordering = ["created_at"]
+
+class UserRoleMapping(models.Model):
+    user=models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+    role=models.ForeignKey(RoleMaster,on_delete=models.CASCADE)
+
+    class Meta:
+        db_table="UserRoleMapping"
+
+
+
+
+
